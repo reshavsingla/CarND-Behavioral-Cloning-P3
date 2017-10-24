7b63378c58445244ed36805a6d9c90db9fff26ab
reshav <reshav@pinguinapp.co>
2017-10-24 10:49:10 -0700

    updated model with generators

--- model.py
+++ model.py
@@ -1,8 +1,12 @@
 import csv
 import cv2
 import numpy as np
+import sklearn
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
 
 lines = []
 with open("./data/driving_log.csv") as csvfile:
@@ -11,25 +15,41 @@ with open("./data/driving_log.csv") as csvfile:
         lines.append(line)
 
 lines.pop(0)
-images = []
-measurements = []
-correction = 0.2
-steering_correction = [0, correction, -correction]
-for line in lines:
-    for i in range(3):
-        image_path = line[i]
-        image_name = image_path.split('/')[-1]
-        source_path = './data/IMG/' + image_name
-        image = cv2.imread(source_path)
-        steering_measurement = float(line[3]) + steering_correction[i]
-        images.append(image)
-        measurements.append(steering_measurement)
-        image_flipped = np.fliplr(image)
-        images.append(image_flipped)
-        measurements.append((-1 * steering_measurement))
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            measurements = []
+            correction = 0.2
+            steering_correction = [0, correction, -correction]
+            for line in batch_samples:
+                for i in range(3):
+                    image_path = line[i]
+                    image_name = image_path.split('/')[-1]
+                    source_path = './data/IMG/' + image_name
+                    image = cv2.imread(source_path)
+                    steering_measurement = float(line[3]) + steering_correction[i]
+                    images.append(image)
+                    measurements.append(steering_measurement)
+                    image_flipped = np.fliplr(image)
+                    images.append(image_flipped)
+                    measurements.append((-1 * steering_measurement))
+
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 
 model = Sequential()
@@ -47,5 +67,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator,
+                    nb_val_samples=len(validation_samples), nb_epoch=5)
 model.save('model.h5')
