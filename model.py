aefe2c32295ebb3fc5a2d6d55b8e78c1c439a570
Reshav <singla.reshav@gmail.com>
2017-10-27 18:21:49 +0000

    correct model

--- model.py
+++ model.py
@@ -9,13 +9,14 @@ from sklearn.utils import shuffle
 
 
 lines = []
-with open("./training_data/driving_log.csv") as csvfile:
+batch = 32
+with open("./data/driving_log.csv") as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
 
 lines.pop(0)
-train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+train_samples, validation_samples = train_test_split(lines, test_size=0.20)
 
 
 def generator(samples, batch_size=32):
@@ -27,13 +28,13 @@ def generator(samples, batch_size=32):
 
             images = []
             measurements = []
-            correction = 0
+            correction = 0.20
             steering_correction = [0, correction, -correction]
             for line in batch_samples:
                 for i in range(3):
                     image_path = line[i]
                     image_name = image_path.split('/')[-1]
-                    source_path = './training_data/IMG/' + image_name
+                    source_path = './data/IMG/' + image_name
                     image = cv2.imread(source_path)
                     steering_measurement = float(line[3]) + steering_correction[i]
                     images.append(image)
@@ -48,13 +49,13 @@ def generator(samples, batch_size=32):
 
 
 # compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+train_generator = generator(train_samples, batch_size=batch)
+validation_generator = generator(validation_samples, batch_size=batch)
 
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Cropping2D(cropping=((60, 20), (0, 0))))
 model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu"))
 model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
 model.add(Conv2D(48, (5, 5), strides=(2, 2), activation="relu"))
@@ -66,11 +67,11 @@ model.add(Dropout(0.5))
 model.add(Dense(50))
 model.add(Dropout(0.5))
 model.add(Dense(10))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, steps_per_epoch=np.ceil(len(train_samples)/32),
-                    validation_data=validation_generator, validation_steps=np.ceil(len(validation_samples)/32),
+model.fit_generator(train_generator, steps_per_epoch=np.ceil(len(train_samples)/batch),
+                    validation_data=validation_generator, validation_steps=np.ceil(len(validation_samples)/batch),
                     epochs=5)
 model.save('model.h5')
